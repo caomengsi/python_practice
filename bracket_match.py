3a4d91690c7cd969558cf0e20f256b4b6cc0d3fd
曹梦思 <237574793@qq.com>
2018-04-21 21:30:51 +0800

    栈在括号匹配中的应用

---
+++ bracket_match.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#栈在括号匹配中的应用
+class Stack:
+    def __init__(self):
+        self.stack=[]
+    def isEmpty(self):
+        return  self.stack==[]
+    def push(self,item):
+        self.stack.append(item)
+    def pop(self):
+        if self.isEmpty():
+            raise IndexError,'pop from empty stack'
+        return  self.stack.pop()
+    def size(self):
+        return len(self.stack)
+def match(i,j):
+    print i,j
+    opens="({["
+    closes=")}]"
+    print opens.index(i)
+    return  opens.index(i)==closes.index(j)
+
+def synstaxChecker(string):
+    balance=True
+    s=Stack()
+    for x in string:
+        if x in "({[":
+            s.push(x)
+        elif x in ")}]":
+            if s.isEmpty():
+                balance=False
+                break
+            else:
+                i=s.pop()
+                if not match(i,x):
+                    balance=False
+                    break
+
+    if not s.isEmpty():
+        balance=False
+    return balance
+print synstaxChecker("{[[]}")
